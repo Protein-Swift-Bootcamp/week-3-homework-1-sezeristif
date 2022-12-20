cdb0b563b8fc610cf29131fc412cef6a70479d91
sezeristif <sezeristif@gmail.com>
2022-12-20 21:44:34 +0300

    Initial Commit

---
+++ bootcamp-week-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bootcamp-week-3
+//
+//  Created by Sezer Istif on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
