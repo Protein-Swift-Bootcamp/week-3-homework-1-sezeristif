5eaa311f54071c7ee7c5591d39e49085d668daf6
sezeristif <sezeristif@gmail.com>
2022-12-20 21:52:16 +0300

    add first and second view controlers

--- bootcamp-week-3/FirstViewController.swift
+++ bootcamp-week-3/FirstViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
